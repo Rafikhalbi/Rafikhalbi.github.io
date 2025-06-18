515a427deca41e1fdbebd96459cdafc9f49f2f12
rafi khalbi <rafiqolbhi@gmail.com>
2025-06-18 19:03:50 +0700

    Portfolio

--- js/script.js
+++ js/script.js
@@ -2,23 +2,23 @@ const burger = document.querySelector('.hamburger-menu');
 const navMenu = document.querySelector('.nav-menu');
 
 burger.addEventListener('click', () => {
-  navMenu.classList.toggle('change');
-});
+  navMenu.classList.toggle('change')
+})
 
 const observer = new IntersectionObserver(
   entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-        observer.unobserve(entry.target);
+        entry.target.classList.add('visible')
+        observer.unobserve(entry.target)
       }
-    });
+    })
   },
   {
     threshold: 0.2,
   }
-);
+)
 
 document.querySelectorAll('.section').forEach(section => {
-  observer.observe(section);
-});
+  observer.observe(section)
+})
